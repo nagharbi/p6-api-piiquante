ec9732b037145c9b4b7ef9d789a23f922724f243
Nouha Gharbi <na.gharbi.94@gmail.com>
2022-09-22 13:42:49 +0200

    sign up

---
+++ controllers/user.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+        const user = new User({
+            email : req.body.email,
+            password: hash
+        });
+
+        user.save()
+        .then(() => res.status(201).json({message : 'utilisateur créé !'}))
+        .catch(error => res.status(400).json({ error}));
+    })
+    .catch(error => res.status(500).json({error}));
+};
+
+// pour connecter utlisateur existant 
+exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                    }
+                    res.status(200).json({
+                        userId: user._id,
+                        token: jwt.sign(    
+                            { userId: user._id },
+                            process.env.SECRET_KEY,
+                            { expiresIn: '24h' }
+                       )
+                    });
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+ }; 
---
+++ routes/user.js
@@ -0,0 +1,9 @@
+
+const express = require('express');
+const router = express.Router();
+const userCtrol = require('../controllers/user');
+
+router.post('/signup', userCtrol.signup);
+router.post('/login', userCtrol.login);
+
+module.exports = router;
